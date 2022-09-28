8e7fdff2325f027ddd60f75a669f651a8e72c363
Saad Salman <61458557+Saad-Salman@users.noreply.github.com>
2022-09-28 11:50:37 +0500

    Add files via upload

---
+++ Mask_RCNN_FullDataset.py
@@ -0,0 +1,430 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 02:27:48 2022
+
+@author: Saad Salman
+"""
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+import cv2
+from mrcnn.visualize import display_instances
+import matplotlib.pyplot as plt
+# Root directory of the project
+ROOT_DIR = os.path.abspath("C:\\Users\\Saad Salman\\Desktop\\Mask-RCNN")
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Directory to save logs and model checkpoints
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+class CustomConfig(Config):
+    """Configuration for training on the dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "object"
+    
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 1
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 7  # Background + (Horse and Man)
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+
+
+
+class CustomDataset(utils.Dataset):
+
+    def load_custom(self, dataset_dir, subset):
+        """Load a subset of the Horse-Man dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("object", 1, "Afraid")
+        self.add_class("object", 2, "Angry")
+        self.add_class("object", 3, "Disgusted")
+        self.add_class("object", 4, "Happy")
+        self.add_class("object", 5, "Neutral")
+        self.add_class("object", 6, "Sad")
+        self.add_class("object", 7, "Surprised")
+        # self.add_class("object", 3, "xyz") #likewise
+
+        # Train or validation dataset?
+        assert subset in ["train", "val","test"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        # Load annotations
+        # VGG Image Annotator saves each image in the form:
+        # { 'filename': '28503151_5b5b7ec140_b.jpg',
+        #   'regions': {
+        #       '0': {
+        #           'region_attributes': {},
+        #           'shape_attributes': {
+        #               'all_points_x': [...],
+        #               'all_points_y': [...],
+        #               'name': 'polygon'}},
+        #       ... more regions ...
+        #   },
+        #   'size': 100202
+        # }
+        # We mostly care about the x and y coordinates of each region
+        annotations1 = json.load(open(os.path.join(dataset_dir, "train_json.json")))
+        # print(annotations1)
+        annotations = list(annotations1.values())  # don't need the dict keys
+
+        # The VIA tool saves images in the JSON even if they don't have any
+        # annotations. Skip unannotated images.
+        annotations = [a for a in annotations if a['regions']]
+        
+        # Add images
+        for a in annotations:
+            # print(a)
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. There are stores in the
+            # shape_attributes (see json format above)
+            polygons = [r['shape_attributes'] for r in a['regions']] 
+            objects = [s['region_attributes']['names'] for s in a['regions']]
+            print("objects:",objects)
+            name_dict = {"Afraid": 1,"Angry": 2,"Disgusted": 3,"Happy": 4,"Neutral": 5,"Sad": 6,"Surprised": 7} #,"xyz": 3}
+            # key = tuple(name_dict)
+            num_ids = [name_dict[a] for a in objects]
+     
+            # num_ids = [int(n['Event']) for n in objects]
+            # load_mask() needs the image size to convert polygons to masks.
+            # Unfortunately, VIA doesn't include it in JSON, so we must read
+            # the image. This is only managable since the dataset is tiny.
+            print("numids",num_ids)
+            image_path = os.path.join(dataset_dir, a['filename'])
+            image = skimage.io.imread(image_path)
+            height, width = image.shape[:2]
+
+            self.add_image(
+                "object",  ## for a single class just add the name here
+                image_id=a['filename'],  # use file name as a unique image id
+                path=image_path,
+                width=width, height=height,
+                polygons=polygons,
+                num_ids=num_ids
+                )
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a Horse/Man dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "object":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        if info["source"] != "object":
+            return super(self.__class__, self).load_mask(image_id)
+        num_ids = info['num_ids']
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        dtype=np.uint8)
+        for i, p in enumerate(info["polygons"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+        	rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+
+        	mask[rr, cc, i] = 1
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        # Map class names to class IDs.
+        num_ids = np.array(num_ids, dtype=np.int32)
+        return mask, num_ids #np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "object":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+#def load_mask(self, image_id):
+#  ...
+#  ##change the for loop only 
+#  for i, p in enumerate(info["polygons"]):
+#    # Get indexes of pixels inside the polygon and set them to 1
+#      if p['name'] == 'polygon':
+#        rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])            
+#      elif p['name'] == 'circle':
+#        rr, cc = skimage.draw.circle(p['cx'], p['cy'], p['r'])
+#      else: 
+#        rr, cc = skimage.draw.ellipse(p['cx'], p['cy'], p['rx'], p['ry'], rotation=np.deg2rad(p['theta']))  
+#        mask[rr, cc, i] = 1
+
+
+def train(model):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = CustomDataset()
+    dataset_train.load_custom("Dataset", "train")
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = CustomDataset()
+    dataset_val.load_custom("Dataset", "val")
+    dataset_val.prepare()
+
+    # *** This training schedule is an example. Update to your needs ***
+    # Since we're using a very small dataset, and starting from
+    # COCO trained weights, we don't need to train too long. Also,
+    # no need to train all layers, just the heads should do it.
+    print("Training network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=50,
+                layers='heads')
+
+
+config = CustomConfig()
+model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=DEFAULT_LOGS_DIR)
+
+weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+if not os.path.exists(weights_path):
+  utils.download_trained_weights(weights_path)
+
+model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+
+train(model)
+
+
+##############################################################################
+
+'Testing'
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.image as mpimg
+# Root directory of the project
+ROOT_DIR = os.path.abspath("")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+from mrcnn import visualize
+from mrcnn.visualize import display_images
+import mrcnn.model as modellib
+from mrcnn.model import log
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+# Path to Ballon trained weights
+# You can download this file from the Releases page
+# https://github.com/matterport/Mask_RCNN/releases
+WEIGHTS_PATH = "logs\\object20220815T1036\\mask_rcnn_object_0029.h5"  # TODO: update this path
+
+
+
+config = CustomConfig()
+CUSTOM_DIR = os.path.join(ROOT_DIR, "")
+class InferenceConfig(config.__class__):
+  # Run detection on one image at a time
+  GPU_COUNT = 1
+  IMAGES_PER_GPU = 1
+  DETECTION_MIN_CONFIDENCE = 0.2
+config = InferenceConfig()
+config.display()
+
+
+
+
+# Device to load the neural network on. Useful if you're training a model on the same machine, in which case use CPU and leave the GPU for training.
+DEVICE = "/gpu:0"  # /cpu:0 or /gpu:0
+# Inspect the model in training or inference modes values: 'inference' or 'training'
+TEST_MODE = "inference"
+def get_ax(rows=1, cols=1, size=16):
+  'Return a Matplotlib Axes array to be used in all visualizations in the notebook. Provide a central point to control graph sizes. Adjust the size attribute to control how big to render images'
+  _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+  return ax
+
+# Load validation dataset
+CUSTOM_DIR = "Dataset"
+dataset = CustomDataset()
+dataset.load_custom(CUSTOM_DIR, "test")
+# Must call before using the dataset
+dataset.prepare()
+print("Images: {}\nClasses: {}".format(len(dataset.image_ids), dataset.class_names))
+
+#LOAD MODEL
+# Create model in inference mode
+with tf.device(DEVICE):
+  model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+  
+  
+ 
+# Load COCO weights Or, load the last model you trained
+weights_path = WEIGHTS_PATH
+# Load weights
+print("Loading weights ", weights_path)
+model.load_weights(weights_path, by_name=True)
+
+
+
+##RUN DETECTION
+#image_id = random.choice(dataset.image_ids)
+#print(image_id)
+#image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+#  modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
+#info = dataset.image_info[image_id]
+#print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], image_id,dataset.image_reference(image_id)))
+## Run object detection
+#results = model.detect([image], verbose=1)
+## Display results
+#x = get_ax(1)
+#r = results[0]
+#visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], dataset.class_names, r['scores'], ax=x, title="Predictions")
+#log("gt_class_id", gt_class_id)
+#log("gt_bbox", gt_bbox)
+#log("gt_mask", gt_mask)
+# This is for predicting images which are not present in dataset
+path_to_new_image = 'Dataset\\test\\Afraid81.jpg'
+image1 = mpimg.imread(path_to_new_image)
+# Run object detection
+print(len([image1]))
+results1 = model.detect([image1], verbose=1)
+# Display results
+ax = get_ax(1)
+r1 = results1[0]
+visualize.display_instances(image1, r1['rois'], r1['masks'], r1['class_ids'],
+dataset.class_names, r1['scores'], ax=ax, title="Predictions1")
+
+
+##############################################################################
+# Compute VOC-style Average Precision
+def compute_batch_ap(image_ids):
+    APs = []
+    for image_id in image_ids:
+        # Load image
+        image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+            modellib.load_image_gt(dataset, config,
+                                   image_id, use_mini_mask=False)
+        # Run object detection
+        results = model.detect([image], verbose=0)
+        # Compute AP
+        r = results[0]
+        AP, precisions, recalls, overlaps =\
+            utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
+                              r['rois'], r['class_ids'], r['scores'], r['masks'])
+        APs.append(AP)
+    return APs
+
+# Pick a set of random images
+image_ids = np.random.choice(dataset.image_ids, 10)
+APs = compute_batch_ap(image_ids)
+print("mAP @ IoU=50: ", np.mean(APs))
+
+##############################################################################
+
+import utils
+
+
+total_gt = np.array([]) 
+total_pred = np.array([]) 
+mAP_ = [] #mAP list
+
+#compute total_gt, total_pred and mAP for each image in the test dataset
+# Compute total ground truth boxes(total_gt) and total predicted boxes(total_pred) and mean average precision for each Image 
+#in the test dataset
+for image_id in dataset.image_ids:
+    image, image_meta, gt_class_id, gt_bbox, gt_mask =modellib.load_image_gt(dataset, config, image_id)#, #use_mini_mask=False)
+    info = dataset.image_info[image_id]
+
+    # Run the model
+    results = model.detect([image], verbose=1)
+    r = results[0]
+    
+    #compute gt_tot and pred_tot
+    gt, pred = utils.gt_pred_lists(gt_class_id, gt_bbox, r['class_ids'], r['rois'])
+    total_gt = np.append(total_gt, gt)
+    total_pred = np.append(total_pred, pred)
+    
+    #precision_, recall_, AP_ 
+    AP_, precision_, recall_, overlap_ = utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
+                                          r['rois'], r['class_ids'], r['scores'], r['masks'])
+    #check if the vectors len are equal
+    print("the actual length of the ground truth vect is : ", len(total_gt))
+    print("the actual length of the predicted vect is : ", len(total_pred))
+    
+    mAP_.append(AP_)
+    print("Average precision of this image : ",AP_)
+    print("The actual mean average precision for the whole images", sum(mAP_)/len(mAP_))
+
+
+import pandas as pd
+total_gt=total_gt.astype(int)
+total_pred=total_pred.astype(int)
+#save the vectors of gt and pred
+save_dir = "output"
+gt_pred_tot_json = {"Total Groundtruth" : total_gt, "predicted box" : total_pred}
+df = pd.DataFrame(gt_pred_tot_json)
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+df.to_json(os.path.join(save_dir,"gt_pred_test.json"))
+
+
+tp,fp,fn=utils.plot_confusion_matrix_from_data(total_gt,total_pred,fz=10, figsize=(20,20), lw=0.5)
+#The vertical axis represent the ground-truth classes and the horizontal axis represent the predicted classes.
+#BG class is the background class.
+# It is not taken into account in the calculation of the mAP.
+
+
+
+# Compute VOC-style Average Precision
+def compute_batch_ap(image_ids):
+    APs = []
+    Recall = []
+    Precision = []
+    for image_id in image_ids:
+        # Load image
+        image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+            modellib.load_image_gt(dataset, config,
+                                   image_id, use_mini_mask=False)
+        # Run object detection
+        results = model.detect([image], verbose=0)
+        # Compute AP
+        r = results[0]
+        AP, precisions, recalls, overlaps =\
+            utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
+                              r['rois'], r['class_ids'], r['scores'], r['masks'])
+        APs.append(AP)
+        Precision.append(precisions)
+        Recall.append(recalls)
+    return APs, Precision, Recall
+
+
+# Pick a set of random images
+#image_ids = np.random.choice(dataset.image_ids, 10)
+APs,Pre,Rec = compute_batch_ap(dataset.image_ids)
+APs
+print("mAP @ IoU=50: ", np.mean(APs),"Precision: ", np.mean(Pre),"Recall: ", np.mean(Rec))
